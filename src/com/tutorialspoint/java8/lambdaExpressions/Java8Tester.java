ec22311a1be0cf481e6a5080e8a0ad895efbb8b8
Daniel-Dos <daniel.dias.developer@gmail.com>
2015-08-01 01:06:46 -0300

    Primeico commit

---
+++ src/com/tutorialspoint/java8/lambdaExpressions/Java8Tester.java
@@ -0,0 +1,32 @@
+//java 8 Lambda Expressions
+//sintaxe  = parameter -> expresison body
+
+/*
+ Following are the important characterisc of a lambda expression
+ 
+ 1 - Optional type declaration = No need to declare type of the
+ parameter. Compiler can inference the same from value of the parameter
+ 
+ 2 - Optional paranthesis around parameter - No need to declare a single parameter in paranthesis. for multile parameters, paranthesis are required.
+ 
+ 3 - Optional curly braces - No need to use curly braces in expresison body if body contains a single statement.
+ 
+ 4 - Optional return keyword = compiler automatically returns value if body has a single expresison to return value. Curly braces are required to indicate that expression returns a value.
+*/
+
+//exempplo
+
+public class Java8Tester {
+
+  public static void main(String[] args) {
+  
+    Java8Tester tester = new Java8Tester();
+	
+	//with type declaration
+	
+  
+  }
+
+
+
+}
